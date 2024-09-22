278dbeffb7899f972d81b6ad8fa5d2acc579bfe3
josefabioguimaraes <jfabioguimaraes@gmail.com>
2024-09-22 12:59:23 -0300

    Feat: Create test_fatorial

--- Calculadora.py
+++ Calculadora.py
@@ -11,9 +11,14 @@ def divisao(a,b):
     return a / b
 
 def fatorial(a):
-    if a == 0:
+    if not isinstance(a, int):
+        raise ValueError("O fatorial de um número não inteiro não existe")
+    if (a < 0):
+        raise ValueError("O fatorial de um número negativo não existe")
+    elif (a == 0):
         return 1
-    return a * fatorial(a-1)
+    else:
+        return a * fatorial(a-1)
 
 def potencia(a,b):
     return a ** b
---
+++ test_fatorial.py
@@ -0,0 +1,22 @@
+import pytest
+from Calculadora import fatorial
+
+def test_fatorial_positivo():
+    assert fatorial(5) == 120
+
+def test_fatorial_zero():
+    assert fatorial(0) == 1
+
+def test_fatorial_maior_que_zero():
+    assert fatorial(5) > 0
+
+def test_fatorial_nao_negativo():
+    assert not fatorial(5) < 0
+
+def test_fatorial_negativo():
+    with pytest.raises(ValueError):
+        fatorial(-5)
+
+def test_fatortial_nao_inteiro():
+    with pytest.raises(ValueError):
+        fatorial(5.5)
